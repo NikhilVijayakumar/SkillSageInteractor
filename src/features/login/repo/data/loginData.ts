6c17ca5379c8636d858b11c4f70ba56af9506095
Nikhil Vijayakumar <nvijayakumar@opentrends.net>
2023-11-10 13:54:08 +0530

    Initial commit

---
+++ src/features/login/repo/data/loginData.ts
@@ -0,0 +1,45 @@
+// src/features/login/store/types/loginTypes.ts
+
+import { AppState } from 'harmony_interactor_kit/src/common/state/appState'
+import {
+  EmailState,
+  PasswordState,
+} from 'harmony_web_kit/src/common/components/form/formData'
+
+type User = {
+  token: string
+  userName: string
+  email: string
+}
+
+export type AuthUser = User | null
+
+export type LoginRequest = {
+  email: string
+  password: string
+}
+
+export type LoginEmailState = EmailState
+export type LoginPasswordState = PasswordState
+
+export interface LoginState<AuthUser>
+  extends EmailState,
+    PasswordState,
+    AppState<AuthUser> {}
+
+export interface LoginResponse {
+  data: {
+    user: AuthUser
+  }
+}
+
+export interface LoginProps {
+  literal: Record<string, string>
+  appstate: LoginState<AuthUser>
+  handleLogin: () => Promise<void>
+  setState: (
+    emailState: LoginEmailState | null,
+    passwordState: LoginPasswordState | null,
+  ) => void
+  setUser: (user: AuthUser) => void
+}
